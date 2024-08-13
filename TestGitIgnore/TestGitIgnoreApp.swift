f72a7e9ca47bcd5810c760ebce582876014d3536
Yaroslav Trach <zhbr282@gmail.com>
2024-08-12 22:28:42 -0400

    Initial Commit

---
+++ TestGitIgnore/TestGitIgnoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitIgnoreApp.swift
+//  TestGitIgnore
+//
+//  Created by User on 2024-08-12.
+//
+
+import SwiftUI
+
+@main
+struct TestGitIgnoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
